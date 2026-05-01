0667c5e41a4a27563480475b9bb0e6b38bed6580
Mehdi <mehdichamam465@gmail.com>
2026-05-01 20:13:38 +0100

    Added Login Controller Allowing compraing Password and Creating Token to verify User

--- controller/login.js
+++ controller/login.js
@@ -2,20 +2,35 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const loginController = async (req,res) =>{
+    try {
     // check if user exist
     const {email,password} = req.body;
-    const existUser = await User.findOne(email);
+    const existUser = await User.findOne({email});
     if(!existUser){
         return res.status(400).json({
             message:"Invalid User"
-        })
+        });
     };
 
     // compare password
-    const isMatch = await bcrypt.compare(password,User.password);
+    const isMatch = await bcrypt.compare(password,existUser.password);
     if(!isMatch){
-        
+        return res.status(400).json({
+            message:"Invalid Password"
+        });
     }
     // give it token 
+    const token = jwt.sign(
+        {id: existUser._id},
+        process.env.JWT_SECRET,
+        {expiresIn:'1d'}
+    );
     // logged in Succesfully
+    res.status(200).json({
+        message:"Logged In Succesfully",
+        token
+    });
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
 }
