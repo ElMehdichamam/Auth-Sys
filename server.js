db88fc54a15bcf48f1cded26ea2ac101ae4850be
Mehdi <mehdichamam465@gmail.com>
2026-05-02 14:51:54 +0100

    Server SetUp

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+const userRouter = require('./router/routers');
+const connectDB = require('./config/db');
 
-app.listen(3000,()=>{
-    console.log('listening On Port 3000')
+
+
+app.use('/api/user',userRouter);
+
+
+connectDB().then(() => {
+  app.listen(3000, () => {
+    console.log('listening on port 3000');
+  });
 });
